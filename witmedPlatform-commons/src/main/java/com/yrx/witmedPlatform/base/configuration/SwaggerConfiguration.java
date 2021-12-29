ea8e1088e2713149e912d0c9c51e73d1db27ae23
DylansGithub <905827723@qq.com>
2021-12-29 22:58:52 +0800

    day1 基础框架搭建

---
+++ witmedPlatform-commons/src/main/java/com/yrx/witmedPlatform/base/configuration/SwaggerConfiguration.java
@@ -0,0 +1,11 @@
+package com.yrx.witmedPlatform.base.configuration;
+
+/**
+ * <b>基础信息功能-swagger API 文档框架</b>
+ *
+ * @author Ydeane
+ * @date 2021-12-29
+ * @since
+ */
+public class SwaggerConfiguration {
+}
